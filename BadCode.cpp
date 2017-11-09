827f67794ef31b3ec7e3ab53ee8b1d7feab7d291
saeed <saeed@ipm.ir>
2017-11-09 11:30:09 +0330

    new branch test refrences

---
+++ BadCode.cpp
@@ -0,0 +1,14 @@
+// first class by : Hamid Shariati
+// version: 0.1
+// date: 9 nov 2017
+// created in vim
+#include <iostream>
+using namespace std;
+int main()
+{
+   int test;
+   int &test2 = test;
+   cout << "hi\n";
+   return 0;
+   
+};
