8160cdf49ee03fd29baa9e0f2f7199459b6a4e0b
saeed <saeed@ipm.ir>
2017-11-09 10:49:06 +0330

    hello world

---
+++ time.cpp
@@ -0,0 +1,12 @@
+// first class by : Hamid Shariati
+// version: 0.1
+// date: 9 nov 2017
+// created in vim
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout << "hi\n";
+   return 0;
+   
+};
